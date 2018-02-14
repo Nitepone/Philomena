682c0a541d1344e12d8c33a48662f53e9c3800e1
Nitepone <admin@night.horse>
2018-02-14 16:26:03 -0500

    https fix

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
       </a>
     </div>
 
-    <script type="text/javascript" src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
     <script type="text/javascript">
       $("#about").click(function(){
         $("#content").load("about.html");
